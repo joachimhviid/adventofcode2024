e2de6d1182f2433fba7b046fa13270b01a529ca2
joachimhviid <joac1099@gmail.com>
2024-12-07 08:12:54 +0100

    Day 3: Part 1 regex

---
+++ day3/main.py
@@ -0,0 +1,10 @@
+from typing import TextIO
+
+
+def main():
+    # Read input data
+    input_data: TextIO = open('input.txt', 'r', encoding='utf-8')
+    regex_str = r'mul\((\d+),(\d+)\)'
+
+if __name__ == '__main__':
+    main()
